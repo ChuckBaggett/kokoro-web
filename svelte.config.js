861bca7cf61eb54d65db5c50507e1fb6f6c47634
ChuckBaggett <chuckbaggettweb@gmail.com>
2025-06-15 04:31:48 -0500

    update netlify cnfiguration

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,31 @@
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
-import adapterNode from "@sveltejs/adapter-node";
-import adapterStatic from "@sveltejs/adapter-static";
+import adapterNetlify from "@sveltejs/adapter-netlify"; // Import the netlify adapter
 
-// https://svelte.dev/docs/kit/adapter-node
-let adapter = adapterNode();
+// The following lines for adapterNode and adapterStatic are commented out
+// as we are now defaulting to adapter-netlify.
+// import adapterNode from "@sveltejs/adapter-node";
+// import adapterStatic from "@sveltejs/adapter-static";
 
-// https://svelte.dev/docs/kit/adapter-static
-if (process.env.ADAPTER === "static") {
-  adapter = adapterStatic({
-    strict: false,
-    fallback: "index.html",
-  });
-}
+// // https://svelte.dev/docs/kit/adapter-node
+// let adapter = adapterNode();
+
+// // https://svelte.dev/docs/kit/adapter-static
+// if (process.env.ADAPTER === "static") {
+//   adapter = adapterStatic({
+//     strict: false,
+//     fallback: "index.html",
+//   });
+// }
+
+// Use adapterNetlify by default
+let adapter = adapterNetlify({
+  // Netlify Edge Functions are enabled by default
+  // edge: true, 
+  // To disable Edge Functions and use regular Netlify Functions:
+  // edge: false,
+  // split: false, // if you don't want to split functions
+});
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -22,7 +35,7 @@ const config = {
 
   kit: {
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-    adapter: adapter,
+    adapter: adapter, // This will now use adapterNetlify by default
 
     // See https://svelte.dev/docs/kit/configuration#env
     env: {
